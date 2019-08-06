cefdbff7dc6d3cd21b8e2025ed7d401dd38cfe3d
ldawson <liamchdawson@gmail.com>
2019-08-06 19:24:53 -0400

    Converted Tile storage to chunks

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -7,16 +7,18 @@ public class GameController : MonoBehaviour
 {
     public GameObject tilePrefab;
 
-    public Tile[] Map;
+    public Chunk[] Map;
 
     Color Entrance = Color.green;
     Color Exit = Color.red;
     Color Left = Color.blue;
     Color color = Color.yellow;
 
-    public int maxY;
-    public int maxX;
-    int i, j;
+    int maxY;
+    int maxX;
+    public int chunkX, chunkY;
+    int i, j; // Current Tile - reset with each tile
+    int u, v; //Current Chunk
     public int seed = 42;
 
 
@@ -28,22 +30,25 @@ public class GameController : MonoBehaviour
 
     void GenerateMap() {
 
-        Map = new Tile[maxX * maxY];
+        Map = new Chunk[1];
+        Map[0] = new Chunk(0, 0, 3, 3);
         Random.InitState(seed);
-        j = maxY - 1;
-        i = Random.Range(0, maxX);
+        j = 2;
+        i = Random.Range(0, 3);
 
         bool bottom;
         bool bottom2;
         bottom = bottom2 = false;
 
-        Add(i, j, 1);
-
+        Map[0].Add(i, j, 1);
+        maxX = Map[0].X;
+        maxY = Map[0].Y;
         while (!bottom2)
         {
             chooseRoom(ref bottom, ref bottom2);
         }
     }
+
     void chooseRoom(ref bool a, ref bool b) {
         float r = Random.Range(0.0f, 1.0f);
         if (r > 0.67f)
@@ -87,7 +92,7 @@ public class GameController : MonoBehaviour
 
         if (j > 0)
         { //Not near bottom
-            Add(i, j, 0);
+            Map[0].Add(i, j, 0);
             b = false;
             return true;
         }
@@ -96,7 +101,7 @@ public class GameController : MonoBehaviour
             if (a)
             { //Already on bottom, exit condition
               //Make Room Exit
-                Add(i, j, 4);
+                Map[0].Add(i, j, 4);
                 b = true;
                 return true;
             }
@@ -104,7 +109,7 @@ public class GameController : MonoBehaviour
             { //Just hit bottom
                 a = true;
                 b = false;
-                Add(i, j, 0);
+                Map[0].Add(i, j, 0);
                 return true;
             }
         }
@@ -114,20 +119,20 @@ public class GameController : MonoBehaviour
     }
 
     bool checkLeft() {
-        if (Map[Index(i - 1, j)] == null)
+        if (Map[0].tiles[Index(i - 1, j)] == null)
         {
             i--;
-            Add(i, j, 0);
+            Map[0].Add(i, j, 0);
             return true;
         }
         return false;
     }
 
     bool checkRight() {
-        if (Map[Index(i + 1, j)] == null)
+        if (Map[0].tiles[Index(i + 1, j)] == null)
         {
             i++;
-            Add(i, j, 0);
+            Map[0].Add(i, j, 0);
             return true;
         }
         return false;
@@ -135,19 +140,21 @@ public class GameController : MonoBehaviour
 
     private void VisualizeMap() {
         Debug.Log("In Visualize");
-        foreach (Tile t in Map) {
-            if (t != null) {
-                GameObject tile = Instantiate(tilePrefab, new Vector3(t.X, t.Y, 0), Quaternion.identity, this.transform);
-                tile.name = $"Tile({t.X}, {t.Y})";
-                if (t.type == TileType.Entrance) tile.GetComponent<SpriteRenderer>().color = Entrance;
-                if (t.type == TileType.Exit) tile.GetComponent<SpriteRenderer>().color = Exit;
+        foreach (Chunk c in Map)
+        {
+            foreach (Tile t in c.tiles)
+            {
+                if (t != null)
+                {
+                    GameObject tile = Instantiate(tilePrefab, new Vector3(t.X, t.Y, 0), Quaternion.identity, this.transform);
+                    tile.name = $"Tile({t.X}, {t.Y})";
+                    if (t.type == TileType.Entrance) tile.GetComponent<SpriteRenderer>().color = Entrance;
+                    if (t.type == TileType.Exit) tile.GetComponent<SpriteRenderer>().color = Exit;
+                }
             }
         }
     }
 
-    void Add(int _i, int _j, int _type) {
-        Map[Index(_i, _j)] = new Tile(_i, _j, _type);
-    }
     int Index(int _i, int _j) {
         return _i + (maxX * _j);
     }
--- Assets/Scripts/Model/Chunk.cs
+++ Assets/Scripts/Model/Chunk.cs
@@ -6,30 +6,28 @@ using System;
 public class Chunk
 {
     //Chunks make up the entirety of the map
-    public int I; //X position of the chunk in the map
-    public int J; //Y position of the chunk in the map
+    public int U; //X position of the chunk in the map
+    public int V; //Y position of the chunk in the map
 
     public int X, Y; //X and Y Dimensions of the chunk
-    public int maxX;
-
 
     public Tile[] tiles;
 
-    public Chunk(int _I, int _J, int _X, int _Y, int _maxX) {
-        I = _I;
-        J = _J;
+    public Chunk(int _U, int _V, int _X, int _Y) {
+        U = _U;
+        V = _V;
         X = _X;
         Y = _Y;
-        maxX = _maxX;
+        tiles = new Tile[X * Y];
     }
 
-    void Add(int _i, int _j, int _type)
+    public void Add(int _i, int _j, int _type)
     {
         tiles[Index(_i, _j)] = new Tile(_i, _j, _type);
     }
 
     int Index(int _i, int _j)
     {
-        return _i + (maxX * _j);
+        return _i + (X * _j);
     }
 }
